6f78665f6527f1a105e2faeecbdc70c1ab34aa50
Renan Góes <renangoes12@gmail.com>
2018-08-07 22:53:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var i = 0;
 function takeANumber (lineNumber,customer){
-  lineNumber[i]= ${i} + 1;
+  lineNumber[i]= i + 1;
   return `Welcome, ${customer}. You are number ${lineNumber.indexOf(customer) + 1} in line.`;
   i++;
 }
