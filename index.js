34baf6d622899f40e181f7892ce05aae8e707d3b
Renan Góes <renangoes12@gmail.com>
2018-08-07 22:57:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var i = 0;
 function takeANumber (lineNumber,customer){
   if (lineNumber.length === 0){
-  lineNumber[i]= customer;
-  return `Welcome, ${customer}. You are number ${lineNumber.indexOf(customer)+1} in line.`
-  i++;
+  lineNumber.unshift = customer;
+  return `Welcome, ${customer}. You are number ${lineNumber.indexOf(customer)+1} in line.`;
   } else {
   lineNumber.push = customer;
   return `Welcome, ${customer}. You are number ${lineNumber.indexOf(customer)+1} in line.`;
