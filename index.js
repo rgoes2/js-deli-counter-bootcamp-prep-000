7a9c090ebd781d7cc1103a7e9612afa4dbbe1bce
Renan Góes <renangoes12@gmail.com>
2018-08-07 22:39:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var deli = true;
 function takeANumber (lineNumber,customer){
 var i = 0;
+if (lineNumber.length > 0 {
+  push.lineNumber = customer;
+}
 while (deli){
   lineNumber [i] = customer;
   return `Welcome, ${customer}. You are number ${lineNumber.indexOf(customer)+1} in line.`;
