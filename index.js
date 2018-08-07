a9859a0f544bd669977f593b7e441a2da994747a
Renan Góes <renangoes12@gmail.com>
2018-08-07 22:45:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 var deli = true;
 function takeANumber (lineNumber,customer){
+  var i = 0;
 while (deli){
-
+  lineNumber[i]= i+1;
   lineNumber.push = customer;
   return `Welcome, ${customer}. You are number ${lineNumber.indexOf(customer)+1} in line.`;
+  i++
   }
 }
-var array = [0];
-console.log(array.length)
